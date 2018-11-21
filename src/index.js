1f6adfef3f978854aca728def2fc47df9ee38346
Vinicius-PC <vinicius.oliveirafonseca@gmail.com>
2018-11-21 00:30:16 -0300

    Projeto Alpha

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //@ts-nocheck
-// module aliases
+// Módulos da biblioteca
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
@@ -7,74 +7,125 @@ var Engine = Matter.Engine,
     Body = Matter.Body,
     Vector = Matter.Vector,
     Runner = Matter.Runner,
-    runningState = false,
-    onTime = null;
 
-// create an engine
+    //Variáveis para nos auxiliar na programação
+    runningState = false,  // Deteca se o ambiente está pausado ou continuando
+    tempo = null,          // Uma função que contará o tempo
+    posicaoAtual = null,   // Uma função para controlar a posição da bola
+    timeSegundos = 0,      // Variável que contará os segundos
+    iniciarState = false   // Variável para detectar se o ambiente iniciou    
+
+
+// Criando o motor do ambiente
 var engine = Engine.create();
 
-// create a renderer
+// Criando o renderizador
 var render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        //Changing the width of canvas
-        width: 1820
+        //Mudando a largura da janela do ambiente
+        width: 1820,
+        height: 700,
+        wireframes: false
     }
 });
 
-// creating a ball and a ground
-const ground = Bodies.rectangle(400, 610, 3000, 60, { isStatic: true, friction: 0});
-const ball = Bodies.circle(10 , 500, 25);
+// Criando o Runner para o controle dinâmico do motor
+var runner = Runner.create({isFixed: true})
 
-// Setting the friction
-ball.friction = 0;
-ball.frictionAir = 0;
+// Criando o chão, as paredes direita, esquerda e a bola
+var ground = Bodies.rectangle(910, 710, 2000, 60, { isStatic: true, friction: 0, render: {
+    fillStyle: 'blue' //Mudando a cor do chão
+}});
 
-//Functions
+var wallLeft = Bodies.rectangle(0, 710, 60, 1500, { isStatic: true, friction: 0, render: {
+    fillStyle: 'blue' //Mudando a cor do chão
+}});
+
+var wallRight = Bodies.rectangle(1820, 710, 60, 1500, { isStatic: true, friction: 0, render: {
+    fillStyle: 'blue' //Mudando a cor do chão
+}});
+
+var roof = Bodies.rectangle(910, 10, 2000, 60, { isStatic: true, friction: 0, render: {
+    fillStyle: 'blue' //Mudando a cor do chão
+}});
+
+var ball = Bodies.circle(910 , 655, 10, {frictionStatic: false, render: {
+    fillStyle: 'red' //Mudando a cor da bola
+}});
+
+var center = Bodies.circle(ball.position.x, ball.position.y, 5, {render: {fillStyle: 'white'}, isStatic: true, isSensor: true})
+
+// Abrindo a janela para visualização do ambiente
+World.add(engine.world, [ball ,ground, wallLeft, wallRight, center, roof]);
+Runner.run(runner, engine);
+Render.run(render);
+
+// Funções utilizadas nos botões
+
+//Botão lançar (launch)
 function launch() {
-    let m = Number.parseFloat(document.getElementById("force").value);;
-    let angle = Number.parseFloat(document.getElementById("angle").value);
-    let mass = Number.parseFloat(document.getElementById("mass").value);
-    World.add(engine.world, [ball ,ground]);
-    Runner.run(engine);
-    Render.run(render);
+    var xForce = Number.parseFloat(document.getElementById("forcex").value);
+    var yForce = Number.parseFloat(document.getElementById("forcey").value);
+    var mass = Number.parseFloat(document.getElementById("mass").value);
     Body.setMass(ball, mass);
-    Body.setAngle(ball, angle);
-    Body.applyForce(ball, Vector.create(1,1), Vector.create(0.01*m,0.01*m));
-    runningState = true;
-    onTime = setInterval(() => {
-        //Velocidade resultante
-        document.getElementById("details").innerText = `Potision: x: ${ball.position.x.toFixed(0)}, y: ${ball.position.y.toFixed(0)}`
-        document.getElementById("timing").innerText = `Time: ${(engine.timing.timestamp/1000).toFixed(0)}`;
-    }, 100)
+    Body.setAngle(ball, 45);
+
+    if (!runningState) {    
+        runningState = true;
+        tempo = setInterval(function(){
+            timeSegundos++;
+            document.getElementById("timing").innerText = "Time: " + timeSegundos;
+        }, 1000);
+
+        posicaoAtual = setInterval(function(){
+            document.getElementById("details").innerText = "Posição: x: " + (ball.position.x - 910).toFixed(0) + "  y: " + ((ball.position.y - 655)*-1).toFixed(0);
+            document.getElementById("timing").innerText = "Time: " + timeSegundos;
+        }, 100);
+    }
+
+Body.applyForce(ball, {x: ball.position.x, y: ball.position.y},{x: xForce/150, y: (yForce/150)*-1})
 }
 
+//Botão reiniciar (reset)
 function reset(){
     location.reload();
 }
 
+//Botão Pausar (pause)
 function pause() {
     if (runningState) {
-        //Pausing the simulation
-        clearInterval(onTime);
+        
+        //Código para parar no instante a simulação
+        clearInterval(tempo);
+        clearInterval(posicaoAtual);
+
         runningState = false;
         ball.isStatic = true;
+
+        Engine.clear(engine);
         Runner.stop(Engine.run(engine));
         return Render.stop(render);
     }
-    // Resetting the simulation
-    setInterval(() => {
-        //Velocidade resultante
-        document.getElementById("details").innerText = `Potision: x: ${ball.position.x.toFixed(0)}, y: ${ball.position.y.toFixed(0)}`
-        document.getElementById("timing").innerText = `Time: ${(engine.timing.timestamp/1000).toFixed(0)}`;
+
+    // Reiniciando a simulação
+    tempo = setInterval(function(){
+        timeSegundos++;
+        document.getElementById("timing").innerText = "Time: " + timeSegundos;
+    }, 1000)
+
+    posicaoAtual = setInterval(function(){
+        document.getElementById("details").innerText = "Posição: x: " + (ball.position.x - 500).toFixed(0) + "  y: " + ((ball.position.y - 655)*-1).toFixed(0);
     }, 100);
+
+
     ball.isStatic = false;
     runningState = true;
     return Render.run(render);
 }
 
-//Events Listerners
+//Adicionando eventos aos botões
 document.getElementById("launch").addEventListener("click", launch, false);
 document.getElementById("pause").addEventListener("click", pause, false);
 document.getElementById("reset").addEventListener("click", reset, false);
